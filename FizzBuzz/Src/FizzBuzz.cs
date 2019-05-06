8f751b8e05222f901a0c425edd93691e075859f7
Yang Jie <jiyang@thoughtworks.com>
2019-05-06 18:26:44 +0800

    [Jane] implement step-2: When the number is a multiple of 3, "Fizz" instead of the number

--- FizzBuzz/Src/FizzBuzz.cs
+++ FizzBuzz/Src/FizzBuzz.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace FizzBuzz
@@ -6,12 +7,26 @@ namespace FizzBuzz
     public class FizzBuzz
     {
         private const int MinValue = 1;
-        
+
         public static IList<string> GetFizzBuzz(int maxValue)
         {
             var numbers = Enumerable.Range(MinValue, maxValue).ToList();
 
-            return numbers.ConvertAll<string>(x => x.ToString());
+            var result = FizzBuzzTranslate(numbers);
+
+            return result;
+        }
+
+        private static IList<string> FizzBuzzTranslate(List<int> numbers)
+        {
+            var result = new List<string>();
+            
+            foreach (var number in numbers)
+            {
+                result.Add(number % 3 == 0 ? "Fizz" : number.ToString());
+            }
+
+            return result;
         }
     }
 }
--- FizzBuzz/Test/FizzBuzzTests.cs
+++ FizzBuzz/Test/FizzBuzzTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace FizzBuzz.Test
@@ -7,11 +8,29 @@ namespace FizzBuzz.Test
         [Fact]
         public void WhenInputNumberShouldGetOneToNumberStringList()
         {
+            //Arrange
             var maxValue = 5;
 
+            //Act
             var result = FizzBuzz.GetFizzBuzz(maxValue);
             
+            //Assert
             Assert.Equal(result.Count, maxValue);
+        }
+        
+        [Fact]
+        public void WhenInput3ShouldGetFizzReplace3InNumberStringList()
+        {
+            //Arrange
+            var maxValue = 6;
+
+            var expect = new List<string> {"1", "2", "Fizz", "4", "5", "Fizz"};
+
+            //Act
+            var result = FizzBuzz.GetFizzBuzz(maxValue);
+            
+            //Assert
+            Assert.Equal(result, expect);
         }
     }
 }
